825aab711d9ac5b11c42559f9ef1ea2f408c50bc
Jon <ijonso123@gmail.com>
2022-09-05 19:47:39 +0800

    update

--- src/main/java/com/abasecode/opencode/base/util/CodeMapUtils.java
+++ src/main/java/com/abasecode/opencode/base/util/CodeMapUtils.java
@@ -1,7 +1,7 @@
 package com.abasecode.opencode.base.util;
 
 import com.abasecode.opencode.base.annotation.NameInMap;
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import org.springframework.cglib.beans.BeanMap;
 
 import javax.servlet.http.HttpServletRequest;
